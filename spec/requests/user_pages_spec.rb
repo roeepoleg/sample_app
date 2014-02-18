b9306a0b656fa27ee547a82a39d243f9af697399
Roee Poleg <roee.poleg@RoeeP.local>
2014-02-18 18:40:27 +0200

    end of chapter 7 ;)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,9 +30,22 @@ describe "UserPages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+
+      before { click_button submit }
+
       it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+        expect { }.not_to change(User, :count)
       end
+
+      it { should have_selector('div.alert.alert-error') }
+
+    end
+
+	describe "after submission" do
+       before { click_button submit }
+
+       it { should have_title('Sign up') }
+       it { should have_content('error') }
     end
 
     describe "with valid information" do
@@ -46,7 +59,15 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
   end
-
 end
