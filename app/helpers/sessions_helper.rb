7216e5f845b2e0bbfe7e7fdab1a782f8d4b80d3a
Roee Poleg <roee.poleg@RoeeP.local>
2014-02-19 14:27:07 +0200

    just before 8.30

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,14 +9,20 @@ module SessionsHelper
 
 	def current_user=(user)
     	@current_user = user
-  	end
+  end
 
-  	def current_user
-      remember_token = User.encrypt(cookies[:remember_token])
-      @current_user ||= User.find_by(remember_token: remember_token)
-  	end
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
 
 	def signed_in?
-    	!current_user.nil?
-  	end
+   	!current_user.nil?
+  end
+
+  def sign_out
+    current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
+    cookies.delete :remember_token
+    current_user = nil
+  end
 end
